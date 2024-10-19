2d2561a7d31448b13ecf57c485bafe5c4130fc34
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-10-19 09:31:34 -0300

    finalização do segundo módulo do curso

--- aprendendoconec.js
+++ aprendendoconec.js
@@ -5,11 +5,50 @@
 const mongoose = require("mongoose")
 
 // Configurando o mongoose
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/loja", {
-    useMongoClient: true,
-}).then(() => {
+// mongoose.Promise = global.Promise; (não é mais necessário a utilização desse comando nas versões mais recentes do mongoose)
+mongoose.connect("mongodb://localhost:27017/loja").then(() => {
     console.log("mongoDB conectado");
 }).catch((err) => {
     console.log("Houve um erro ao se conectar ao mongoDB: " + err);
 });
+
+// Model - Usuários
+// Definindo um model
+const UsuarioSchema  = mongoose.Schema({
+
+    nome: {
+        type: String,
+        require: true // campo obrigatório
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number, // não é int, ao utilizar para números como idada
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+// Collection
+mongoose.model('usuarios', UsuarioSchema) 
+
+
+const Vitoria = mongoose.model('usuarios')
+new Vitoria({
+    nome: "Vitória",
+    sobrenome: "Garrucho",
+    email: "vitoriagarrucho@gmail.com",
+    idade: 24,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Usuário criado com sucesso")
+}).catch((err) => {
+    console.log("Houve um erro ao registar o usuário: +err")
+})
